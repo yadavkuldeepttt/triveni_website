ef1fc18429fdf1442dc86b7b82caee0eddea87fa
duser80 <dprogrammar380@gmail.com>
2025-03-27 22:09:34 +0530

    Update index.js
    helmet provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from "react-router";
 import { createRoot } from 'react-dom/client';
-
+import {  HelmetProvider } from 'react-helmet-async';
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
+   <HelmetProvider>
   <BrowserRouter>
-<App />
-</BrowserRouter>
+    <App />
+  </BrowserRouter>
+</HelmetProvider>
 );
